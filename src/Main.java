eed293b623c888b0047964c8768fe22ab086fa7a
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-17 10:14:46 +0900

    Lambda Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+@FunctionalInterface
+interface MathOperation{
+    int operate(int a, int b);
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation multiply = (a, b) -> a * b;
+
+        System.out.println(add.operate(2,5));
+        System.out.println(multiply.operate(2 ,4));
+    }
+}
